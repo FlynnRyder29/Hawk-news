e562cdbf67b8ffc995c703f533a0ab4c78b9c2b1
Piyush Singh <piyushsingh3060@gmail.com>
2025-07-10 23:19:51 +0530

    added pagination

--- src/Components/News.js
+++ src/Components/News.js
@@ -14,13 +14,38 @@ export class News extends Component {
   }
 
   async componentDidMount(){
-    let url="https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=d590dec237464665828f9e1ca2994593";
+    let url="https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=d590dec237464665828f9e1ca2994593&page=1&pageSize=15";
     let data = await fetch(url);
     let parsedData=await data.json()
     this.setState({
       articles:parsedData.articles||[],
       loading:false,
     })
+  }
+  handleNext=async()=>{
+    if (this.state.page+1>Math.ceil(this.state.totalResults/15)){
+
+    }
+    else{
+    let url=`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=d590dec237464665828f9e1ca2994593&page=${this.state.page+1}pageSize=15`;
+    let data=await fetch(url);
+    let parsedData=await data.json();
+    this.setState({
+      page:this.state+1,
+      articles:parsedData.articles||[]
+    })
+  }
+  }
+  
+  handlePrevious=async()=>{
+    let url=`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=d590dec237464665828f9e1ca2994593&page=${this.state.page-1}pageSize=15`;
+    let data=await fetch(url);
+    let parsedData=await data.json();
+    this.setState({
+      page:this.state+1,
+      articles:parsedData.articles||[]
+    })
+
   }
 
   render() {
@@ -33,6 +58,13 @@ export class News extends Component {
             <Newsitem title={element.title?element.title.slice(0,45):""} description={element.description?element.description.slice(0,88):""} imageUrl={element.urlToImage||"https://about.fb.com/wp-content/uploads/2023/09/GettyImages-686732223.jpg"} newsUrl={element.url}/>
           </div>
           })}
+        </div>
+
+        <div className="container d flex justify-content-between my-3">
+          <button disabled={this.state.page<=1} type="button"
+          className='btn btn dark' onClick={this.handlePrevious}>&larr; Previous</button>
+            
+          <button disabled={this.state.page+1>Math.ceil(this.state.totalResults/15)} type="button" className="btn btn dark" onClick={this.handleNext}>Next &rarr;</button>
         </div>
       </div>
     )
