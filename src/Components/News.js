ef377b9d7094734b2cf22aa809882ba18a2136cd
Piyush Singh <piyushsingh3060@gmail.com>
2025-07-30 23:21:36 +0530

    Added Infinite Pagination feature and removed prev and next

--- src/Components/News.js
+++ src/Components/News.js
@@ -3,6 +3,8 @@ import Newsitem from './Newsitem'
 import './News.css';
 import Spinner from './Spinner';
 import PropTypes from 'prop-types';
+import InfiniteScroll from 'react-infinite-scroll-component';
+
 export class News extends Component {
 
   static defaultProps={
@@ -28,18 +30,18 @@ export class News extends Component {
     document.title=`HawK News -${this.props.category.charAt(0).toUpperCase()+this.props.category.slice(1)}`
   }
 
-  async fetchNews(page){
-    this.setState({loading:true,error:null});
+  async fetchNews(page,append=false) {
+    this.setState({loading:!append,error:null});
     try{
       let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d590dec237464665828f9e1ca2994593&page=${page}&pageSize=${this.props.pageSize || 15}`;
       let data=await fetch(url);
       let parsedData=await data.json();
-      this.setState({
-        articles:parsedData.articles||[],
+      this.setState(prevState =>({
+        articles:append? [...prevState.articles, ...parsedData.articles] : parsedData.articles,
         totalResults:parsedData.totalResults||0,
         loading:false,
         page,
-      });
+      }));
     }catch(error){
       this.setState({loading:false,error:"Failed to fetch news articles. Please try again later."});
     }
@@ -70,28 +72,73 @@ export class News extends Component {
 
   }
 
+  fetchMoreData = async()=> { 
+    
+    const nextPage = this.state.page + 1;
+    if (this.state.articles.length >= this.state.totalResults) {
+      return;
+    }
+    await this.fetchNews(nextPage, true);
+  }
+
   render() {
+    const { articles, loading, error, totalResults } = this.state;
+    
     return (
-      <div className='container my-3'>
-        {this.state.loading && <Spinner />}
+      <>
+        {/* Show spinner only for initial loading */}
+        {loading && articles.length === 0 && <Spinner />}
         <h1 className='text-center'>HawK News</h1>
         <h2 className='text-center'>Top {this.props.category.charAt(0).toUpperCase()+this.props.category.slice(1)} Headlines</h2>
-        <div className="row">
-          {!this.state.loading&&this.state.articles.map((element)=>{
-          return <div className="col-md-4" key={element.url||element.title}>
-            <Newsitem title={element.title?element.title.slice(0,45):""} description={element.description?element.description.slice(0,88):""} imageUrl={element.urlToImage||"https://about.fb.com/wp-content/uploads/2023/09/GettyImages-686732223.jpg"} newsUrl={element.url} author={element.author} date={element.publishedAt}/>
+        
+        {/* Show error message if there's an error */}
+        {error && (
+          <div className="alert alert-danger text-center" role="alert">
+            {error}
           </div>
-          })}
-        </div>
-
-        <div className="news-pagination">
-          <button disabled={this.state.page<=1} type="button"
-          className='button-pagination' onClick={this.handlePrevious}>&larr; Previous</button>
-            
-          <button disabled={this.state.page+1>Math.ceil(this.state.totalResults/(this.props.pageSize||15))} type="button" className="button-pagination" onClick={this.handleNext}>Next &rarr;</button>
-        </div>
-      </div>
+        )}
+        
+        <InfiniteScroll
+          dataLength={articles.length}
+          next={this.fetchMoreData}
+          hasMore={articles.length < totalResults}
+          loader={<div className="text-center my-3"><Spinner /></div>}
+          endMessage={
+            <div className="text-center my-4">
+              <p style={{ 
+                color: '#ca9204', 
+                fontSize: '1.2rem', 
+                fontWeight: 'bold',
+                fontFamily: 'Times New Roman, serif'
+              }}>
+                📰 You've reached the end of today's headlines! 📰
+              </p>
+            </div>
+          }
+        >
+        <div className='container my-3'>  
+          <div className="row">
+            {articles.map((element, index) => {
+              return (
+                <div className="col-md-4" key={`${element.url}-${index}` || `${element.title}-${index}`}>
+                  <Newsitem 
+                    title={element.title ? element.title.slice(0,45) : ""} 
+                    description={element.description ? element.description.slice(0,88) : ""} 
+                    imageUrl={element.urlToImage || "https://about.fb.com/wp-content/uploads/2023/09/GettyImages-686732223.jpg"} 
+                    newsUrl={element.url} 
+                    author={element.author} 
+                    date={element.publishedAt}
+                  />
+                </div>
+              );
+            })}
+          </div>
+          </div>
+        </InfiniteScroll>
+      </>
+    
     )
+    
   }
 }
 
